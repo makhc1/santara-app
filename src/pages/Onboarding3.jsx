c590ceadbd9da520e5f5d024ccda11e71a12d921
ahm4d-putra <ahmadputrar03@gmail.com>
2026-05-21 14:22:39 +0700

    fix: onboarding layer

--- src/pages/Onboarding3.jsx
+++ src/pages/Onboarding3.jsx
@@ -16,13 +16,7 @@ const Onboarding3 = () => {
             alt="Onboarding 3"
             className="w-full h-full object-cover"
           />
-          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10">
-            <div className="w-40 h-72 bg-gray-900 rounded-[30px] border-4 border-gray-800 shadow-xl flex items-center justify-center">
-              <div className="w-36 h-64 bg-white rounded-[24px] overflow-hidden flex items-center justify-center text-muted text-xs">
-                App Preview
-              </div>
-            </div>
-          </div>
+          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10"></div>
         </div>
 
         <div className="flex-1 bg-white rounded-t-3xl -mt-6 p-6 flex flex-col justify-between relative z-10 shadow-[0_-10px_30px_rgba(0,0,0,0.05)]">
